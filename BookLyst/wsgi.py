2f8b807825f382edff576e70a2f79b3301034c51
MirandaWopps <lucas.pereiraskeit@gmail.com>
2024-11-24 12:58:07 -0300

    nova versao pulando antes do slide 11

--- BookLyst/wsgi.py
+++ BookLyst/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BookLyst.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BookLyst.settings')
 
 application = get_wsgi_application()
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,11 +1,5 @@
-from django.urls import path
-from accounts import views
-from django.urls import include
-
 app_name = 'accounts'
 
 urlpatterns = [
-    path('accountsClasse/', views.accountsClasse.as_view(), name='accountsClasse'),
-    path('accountsGET/', views.accountsGET, name='accountsGET'),
-    path('accountsPOST/', views.accountsPOST, name='accountsPOST'),
+    
 ]
